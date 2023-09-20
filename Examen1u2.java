918d52f7f6d71a4f7205bf02bad302699e39546b
Lalofuja <143622730+Lalofuja@users.noreply.github.com>
2023-09-20 08:45:44 -0600

    Add files via upload

---
+++ Examen1u2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1u2;
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class Examen1u2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner lectura = new Scanner (System.in);
+       //Precios de venta
+        double guitarra= 1250.45;
+        double trompeta= 3743.00;
+        double flauta= 2683.78;
+        
+       
+        System.out.println("Ingresa un instrumento: ");
+        double Instrumento = lectura.nextInt();
+        
+        System.out.println("Ingresa el precio de compra: ");
+        double preciodecompra = lectura.nextDouble();
+        
+        
+        
+       
+        
+        
+        
+    }
+    
+}
